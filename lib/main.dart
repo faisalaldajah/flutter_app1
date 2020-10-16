0efbe7a94e5657fd7dec4605ce18a794edd99e60
faisalaldajah <32023498+faisalaldajah@users.noreply.github.com>
2020-10-16 22:29:05 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,15 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.green,
       ),
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                height: 80,
-              ),
-              Text(
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Positioned(
+              child: Text(
                 'Create a New Account',
                 style: TextStyle(
                   color: Colors.black,
@@ -27,128 +26,149 @@ class MyApp extends StatelessWidget {
                   //fontFamily: 'krona'
                 ),
               ),
-              Text('phone',style: TextStyle(fontSize: 20),),
-              Container(
-                width: 300,
-                //width: MediaQuery.of(context).size.width - 100,
-                height: 50,
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(width: 20),
-                    Icon(Icons.phone_android),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text('+962 123 456 78',style: TextStyle(fontSize: 20),)
-                  ],
+            ),
+          ),
+          Text(
+            'phone',
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 300,
+            //width: MediaQuery.of(context).size.width - 100,
+            height: 50,
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                SizedBox(width: 20),
+                Icon(Icons.phone_android),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  '+962 123 456 78',
+                  style: TextStyle(fontSize: 20),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            child: Divider(
+              color: Colors.green,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Email Address',
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 300,
+            height: 50,
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                SizedBox(width: 20),
+                Icon(Icons.mail),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'abcdef@gmail.com',
+                  style: TextStyle(fontSize: 20),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            child: Divider(
+              color: Colors.green,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'password',
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            width: 300,
+            height: 50,
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                SizedBox(width: 65),
+                Text(
+                  '*****************',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Icon(
+                  Icons.remove_red_eye,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            child: Divider(
+              color: Colors.green,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 300,
+            height: 50,
+            color: Colors.green,
+            child: Center(
+              child: Text(
+                'SignUp',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
                 ),
               ),
-              Container(
-                width: 300,
-                child: Divider(
-                  color: Colors.green,
-                ),
+            ),
+          ),
+          SizedBox(height: 25),
+          Text('or sign up with'),
+          SizedBox(
+            height: 25,
+          ),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: 80,
+              ),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/fb.png'),
+                radius: 30,
               ),
               SizedBox(
-                height: 10,
+                width: 20,
               ),
-              Text('Email Address',style: TextStyle(fontSize: 20),),
-              Container(
-                width: 300,
-                height: 50,
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(width: 20),
-                    Icon(Icons.mail),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text('abcdef@gmail.com',style: TextStyle(fontSize: 20),)
-                  ],
-                ),
-              ),
-              Container(
-                width: 300,
-                child: Divider(
-                  color: Colors.green,
-                ),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/insta.png'),
+                radius: 30,
               ),
               SizedBox(
-                height: 10,
+                width: 20,
               ),
-              Text('password',style: TextStyle(fontSize: 20),),
-              Container(
-                width: 300,
-                height: 50,
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(width: 65),
-                    Text('*****************',style: TextStyle(fontSize: 20),),
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Icon(
-                      Icons.remove_red_eye,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: 300,
-                child: Divider(
-                  color: Colors.green,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: 300,
-                height: 50,
-                color: Colors.green,
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      'SignUp',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 100,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 25),
-              Text('or sign up with'),
-              SizedBox(height: 25,),
-              Row(
-                children: <Widget>[
-                  SizedBox(width: 80,),
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/fb.png'),
-                    radius: 30,
-                  ),
-                  SizedBox(width: 20,),
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/insta.png'),
-                    radius: 30,
-                  ),
-                  SizedBox(width: 20,),
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/google.webp'),
-                    radius: 30,
-                  )
-                ],
-              ),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/google.webp'),
+                radius: 30,
+              )
             ],
           ),
-        ),
+        ],
       ),
     );
   }
